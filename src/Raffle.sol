0e5c34a2abfe51eaf49ec0f798101d1474b7def3
piedro1979 <piedro@gmx.at>
2025-02-18 17:38:35 +0100

    Intro to VRFv2.5

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -39,18 +39,38 @@ pragma solidity ^0.8.18;
 error Raffle__NotEnoughEthSent();
 
 contract Raffle {
+    
+    /** State Variables */
     uint256 private immutable i_entranceFee;
+    // @dev Duration of the lottery in seconds
+    uint256 private immutable i_interval;
+    address payable[] private s_players;
+    uint256 private s_lastTimeStamp;
+    
 
-    constructor(uint256 entranceFee) {
+    /** Events */
+    event EnteredRaffle(address indexed player);
+
+    /** Constructor */
+        constructor(uint256 entranceFee, uint256 interval) {
         i_entranceFee = entranceFee;
+        i_interval = interval;
+        s_lastTimeStamp = block.timestamp;
     }
+
     function enterRaffle() external payable {
         // require(msg.value >= i_entranceFee, "Not enough ETH sent");
         if(msg.value >= i_entranceFee) revert Raffle__NotEnoughEthSent();
+        s_players.push(payable(msg.sender));
+        emit EnteredRaffle(msg.sender);
     }
 
-    function pickWinner() public {
-        
+    // 1. Get a random number
+    // 2. Use the random number to pick a player
+    // 3. Automatically called
+    function pickWinner() external {
+        // check to see if enough time has passed
+        if(block.timestamp - s_lastTimeStamp < i_interval) revert();
     }
 
     /** Getter Functions */
