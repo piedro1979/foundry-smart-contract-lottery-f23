c7bb06db2d4a5b2d5e737aac32cd02c03c906733
piedro1979 <piedro@gmx.at>
2025-02-19 17:03:40 +0100

    ChainLink VRF integration

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -34,12 +34,21 @@ pragma solidity ^0.8.18;
  */
 
 /** Imports */
+import {VRFCoordinatorV2Interface} from "chainlink/src/v0.8/vrf/interfaces/VRFCoordinatorV2Interface.sol";
+import {VRFConsumerBaseV2} from "chainlink/src/v0.8/vrf/VRFConsumerBaseV2.sol";
 
 /** Errors */
 error Raffle__NotEnoughEthSent();
 
-contract Raffle {
-    
+contract Raffle is VRFConsumerBaseV2 {
+    /** Chainlink VRF related variables */
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    bytes32 private immutable i_gasLane;
+    uint64 private immutable i_subscriptionId;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private immutable i_callbackGasLimit;
+    uint32 private constant NUM_WORDS = 1;
+
     /** State Variables */
     uint256 private immutable i_entranceFee;
     // @dev Duration of the lottery in seconds
@@ -52,10 +61,15 @@ contract Raffle {
     event EnteredRaffle(address indexed player);
 
     /** Constructor */
-        constructor(uint256 entranceFee, uint256 interval) {
+        constructor(uint256 entranceFee, uint256 interval, address vrfCoordinator, bytes32 gasLane, uint64 subscriptionId, uint32 callbackGasLimit) VRFConsumerBaseV2(vrfCoordinator) {
         i_entranceFee = entranceFee;
         i_interval = interval;
         s_lastTimeStamp = block.timestamp;
+
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinator);
+        i_gasLane = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
     }
 
     function enterRaffle() external payable {
@@ -70,9 +84,19 @@ contract Raffle {
     // 3. Automatically called
     function pickWinner() external {
         // check to see if enough time has passed
-        if(block.timestamp - s_lastTimeStamp < i_interval) revert();
-    }
+        if(block.timestamp - s_lastTimeStamp < i_interval) revert(); 
 
+        uint256 requestId = i_vrfCoordinator.requestRandomWords(
+            i_gasLane,
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS,
+            i_callbackGasLimit,
+            NUM_WORDS
+        );
+    }   
+
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override {}
+    
     /** Getter Functions */
     function getEntranceFee() external view returns(uint256){
         return i_entranceFee;
